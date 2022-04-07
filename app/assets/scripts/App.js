b787449d3d0936fdb3bd078ea1d5fa724088a5ef
Rapha <raphaelle.delmas@lilo.org>
2022-04-07 19:26:55 +0900

    finished css section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,19 @@ import '../styles/styles.css'
 if (module.hot) {
     module.hot.accept()
 };
+
+/* Lesson exapmle code below this line */
+
+function Person(fullName, favColor) {
+    this.name = fullName;
+    this.favoriteColor = favColor;
+    this.greet = function () {
+        console.log(`Hello, my name is ${this.name} and my favorite is color is ${this.favoriteColor}.`);
+    }
+}
+
+let john = new Person("John Doe", "blue");
+john.greet();
+
+let jane = new Person("Jane Smith", "green");
+jane.greet();
