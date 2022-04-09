653e86e6e8ce45ceca779bb76b30aa36e34fa73f
Rapha <raphaelle.delmas@lilo.org>
2022-04-10 00:22:15 +0900

    start work on modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,9 +3,11 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 import HighlightedLinks from './modules/HighlightedLinks';
+import Modal from './modules/Modal'
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+new Modal();
 
 let mobileMenu = new MobileMenu();
 
