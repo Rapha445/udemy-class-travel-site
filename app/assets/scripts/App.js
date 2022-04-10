64ceb14271c2a04a168395f7238e1dbd314bc638
Rapha <raphaelle.delmas@lilo.org>
2022-04-10 10:48:19 +0900

    finished modal functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,17 +3,27 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 import HighlightedLinks from './modules/HighlightedLinks';
-import Modal from './modules/Modal'
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
-new Modal();
+new MobileMenu();
+new StickyHeader();
+new HighlightedLinks();
 
-let mobileMenu = new MobileMenu();
-
-let stickyHeader = new StickyHeader();
-
-let highlightedLinks = new HighlightedLinks();
+let modal;
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault();
+        if (typeof modal == "undefined") {
+            import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
+                modal = new x.default();
+                setTimeout(() => modal.openTheModal(), 20);
+            }).catch(() => console.log("error"));
+        } else {
+            modal.openTheModal()
+        }
+    })
+})
 
 if (module.hot) {
     module.hot.accept()
--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -3,12 +3,9 @@ class Modal {
         this.injectHTML();
         this.modal = document.querySelector(".modal");
         this.closeIcon = document.querySelector(".modal__close");
-        this.openModalButtons = document.querySelectorAll(".open-modal");
         this.events();
     }
     events() {
-        // listen for open click
-        this.openModalButtons.forEach(el => el.addEventListener("click", e => this.openTheModal(e)));
         //listen for close click
         this.closeIcon.addEventListener("click", () => this.closeTheModal());
         //pushes any key
@@ -19,8 +16,7 @@ class Modal {
             this.closeTheModal();
         }
     }
-    openTheModal(e) {
-        e.preventDefault();
+    openTheModal() {
         this.modal.classList.add("modal--is-visible");
     }
     closeTheModal() {
